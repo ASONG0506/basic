e2e6bb391f43485326927579fcd8739ae25c9b5d
chenjiansong <chenjiansong@trunk.tech>
2023-12-30 12:00:00 +0800

    <feat>: add image channel invert algo

---
+++ 01cuda_resize/demo_color_channel_invert.cu
@@ -0,0 +1,64 @@
+#include <cuda_runtime.h>
+#include <opencv2/opencv.hpp>
+
+__global__ void colorChannelConvertKernel(const uchar3* input, uchar3* output, int width, int height, int offset){
+    int x = blockIdx.x*blockDim.x+threadIdx.x;
+    int y = blockIdx.y*blockDim.y+threadIdx.y;
+
+    // process color
+    if (x<width && y<height){
+        int index = y*width+x;
+        uchar3 color = input[index];
+
+        // convert color
+        auto tmp=color.x;
+        color.x=color.z;
+        color.z=tmp;
+        output[index]=color;
+    }
+}
+
+
+void colorChannelConvertCUDA(const uchar3* h_input, uchar3* h_output, int width, int height, int offset) {
+    int size = width * height * sizeof(uchar3);
+
+    uchar3* d_input, *d_output;
+    cudaMalloc((void**)&d_input, size);
+    cudaMalloc((void**)&d_output, size);
+
+    cudaMemcpy(d_input, h_input, size, cudaMemcpyHostToDevice);
+
+    dim3 blockSize(16, 16);
+    dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+
+    colorChannelConvertKernel<<<gridSize, blockSize>>>(d_input, d_output, width, height, offset);
+
+    cudaMemcpy(h_output, d_output, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
+
+int main() {
+    cv::Mat image = cv::imread("../input.jpg");
+    
+    if (image.empty()) {
+        std::cerr << "Error: Could not read the image." << std::endl;
+        return -1;
+    }
+
+    int width = image.cols;
+    int height = image.rows;
+
+    uchar3* h_input = (uchar3*)image.data;
+    uchar3* h_output = new uchar3[width * height];
+    int offset=0;
+    colorChannelConvertCUDA(h_input, h_output, width, height, offset);
+
+    cv::Mat output_image(height, width, CV_8UC3, h_output);
+    cv::imwrite("output.jpg", output_image);
+
+    delete[] h_output;
+
+    return 0;
+}
